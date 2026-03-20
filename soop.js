90499a8272ce91ae0d82cfc60c32428e798e01fd
wkenjadu <121525130+wkenjadu@users.noreply.github.com>
2026-03-21 02:23:05 +0900

    Update soop.js

--- soop.js
+++ soop.js
@@ -61,7 +61,7 @@ async function checkStream() {
     );
 
     const broadData = res.data?.broad;
-    const isLive = broadData ? (broadData.is_onair === "Y") : false;
+    const isLive = true;
 
     console.log(`[체크] 방송 상태: ${isLive ? "ON" : "OFF"}`);
     console.log("카테고리 관련 값:", {
